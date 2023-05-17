190f411478105fec0d2ac603a85dbf743d0c801c
Bryan Spitz <bryanjspitz@gmail.com>
2023-05-17 15:04:22 -0400

    Created recipe card layout

--- app/src/main/java/com/bryanspitz/recipes/ui/main/MainLayout.kt
+++ app/src/main/java/com/bryanspitz/recipes/ui/main/MainLayout.kt
@@ -3,7 +3,6 @@ package com.bryanspitz.recipes.ui.main
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -20,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bryanspitz.recipes.R
@@ -59,10 +57,11 @@ fun MainLayout(
             verticalArrangement = spacedBy(16.dp)
         ) {
             itemsIndexed(recipes, { _, item -> item }) { index, item ->
-                Text(
-                    text = item,
-                    textAlign = if (index % 2 == 0) TextAlign.Start else TextAlign.End,
-                    modifier = Modifier.fillMaxWidth()
+                RecipeCard(
+                    title = item,
+                    description = "This is the $item",
+                    imgUrl = "",
+                    imageAtEnd = index % 2 == 0
                 )
             }
         }
---
+++ app/src/main/java/com/bryanspitz/recipes/ui/main/RecipeCard.kt
@@ -0,0 +1,88 @@
+package com.bryanspitz.recipes.ui.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bryanspitz.recipes.ui.theme.RecipesTheme
+
+@Composable
+fun RecipeCard(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    imgUrl: String,
+    imageAtEnd: Boolean = true
+) {
+    ElevatedCard(modifier = modifier) {
+        Row {
+            if (!imageAtEnd) {
+                Box(
+                    modifier = Modifier
+                        .weight(40f)
+                        .aspectRatio(1f)
+                        .background(Color.White)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .weight(60f),
+                verticalArrangement = spacedBy(8.dp)
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(text = description)
+            }
+            if (imageAtEnd) {
+                Box(
+                    modifier = Modifier
+                        .weight(40f)
+                        .aspectRatio(1f)
+                        .background(Color.White)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewRecipeCardRight() {
+    RecipesTheme {
+        RecipeCard(
+            modifier = Modifier.fillMaxWidth(),
+            title = "Pflaumenkuchen",
+            description = "A traditional German plum cake.",
+            imgUrl = ""
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewRecipeCardLeft() {
+    RecipesTheme {
+        RecipeCard(
+            modifier = Modifier.fillMaxWidth(),
+            title = "Pflaumenkuchen",
+            description = "A traditional German plum cake.",
+            imgUrl = "",
+            imageAtEnd = false
+        )
+    }
+}
