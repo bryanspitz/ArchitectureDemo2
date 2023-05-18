c0b5d318844f0dab5ff96bf14df819372ab439b1
Bryan Spitz <bryanjspitz@gmail.com>
2023-05-18 16:42:44 -0400

    Added shot library

--- app/src/androidTest/java/com/bryanspitz/recipes/test/RecipesTestRunner.kt
+++ app/src/androidTest/java/com/bryanspitz/recipes/test/RecipesTestRunner.kt
@@ -4,9 +4,10 @@ import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import com.bryanspitz.recipes.app.TestRecipesApplication
+import com.karumi.shot.ShotTestRunner
 
 @Suppress("unused")
-class RecipesTestRunner: AndroidJUnitRunner() {
+class RecipesTestRunner: ShotTestRunner() {
 
     override fun newApplication(
         cl: ClassLoader?,
---
+++ app/src/androidTest/java/com/bryanspitz/recipes/ui/main/MainActivityTest.kt
@@ -0,0 +1,34 @@
+package com.bryanspitz.recipes.ui.main
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.core.app.ActivityScenario
+import com.karumi.shot.ActivityScenarioUtils.waitForActivity
+import com.karumi.shot.ScreenshotTest
+import org.junit.Rule
+import org.junit.Test
+
+internal class MainActivityTest : ScreenshotTest {
+
+    @Rule
+    @JvmField
+    val composeRule = createComposeRule()
+
+    @Test
+    fun rendersTheDefaultActivityState() {
+        val activity = ActivityScenario.launch(MainActivity::class.java)
+
+        compareScreenshot(activity.waitForActivity())
+    }
+
+    @Test
+    fun composeTest() {
+        composeRule.setContent {
+            MainLayout(
+                recipes = emptyList(),
+                onAdd = {}
+            )
+        }
+
+        compareScreenshot(composeRule)
+    }
+}
