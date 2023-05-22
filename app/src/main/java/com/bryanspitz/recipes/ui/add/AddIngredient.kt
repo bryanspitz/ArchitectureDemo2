a2b66237c31a1c33b41c6daae50639eda4b5ce03
Bryan Spitz <bryanjspitz@gmail.com>
2023-05-22 11:10:47 -0400

    Added keyboard actions to ingredient and instruction rows.

--- app/src/main/java/com/bryanspitz/recipes/ui/add/AddIngredient.kt
+++ app/src/main/java/com/bryanspitz/recipes/ui/add/AddIngredient.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,20 +50,27 @@ fun AddIngredient(
                     modifier = Modifier.width(80.dp),
                     label = { Text(stringResource(R.string.amount)) },
                     keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Number
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
                     )
                 )
                 OutlinedTextField(
                     value = unit,
                     onValueChange = { onChanged(copy(unit = it)) },
                     modifier = Modifier.width(80.dp),
-                    label = { Text(stringResource(R.string.unit)) }
+                    label = { Text(stringResource(R.string.unit)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    )
                 )
                 OutlinedTextField(
                     value = name,
                     onValueChange = { onChanged(copy(name = it)) },
                     modifier = Modifier.weight(1f),
-                    label = { Text(stringResource(R.string.name)) }
+                    label = { Text(stringResource(R.string.name)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    )
                 )
             }
             Row(
@@ -72,7 +81,11 @@ fun AddIngredient(
                     value = preparation,
                     onValueChange = { onChanged(copy(preparation = it)) },
                     modifier = Modifier.weight(1f),
-                    label = { Text(stringResource(R.string.preparation)) }
+                    label = { Text(stringResource(R.string.preparation)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(onDone = { onSave() })
                 )
                 IconButton(onClick = onSave) {
                     Icon(
--- app/src/main/java/com/bryanspitz/recipes/ui/add/AddInstruction.kt
+++ app/src/main/java/com/bryanspitz/recipes/ui/add/AddInstruction.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Done
@@ -19,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bryanspitz.recipes.R
@@ -48,7 +51,11 @@ fun AddInstruction(
                 onValueChange = { onChanged(instruction.copy(instruction = it)) },
                 modifier = Modifier
                     .weight(1f)
-                    .alignByBaseline()
+                    .alignByBaseline(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = { onSave() })
             )
         }
         Row(modifier = Modifier.padding(start = 32.dp)) {
